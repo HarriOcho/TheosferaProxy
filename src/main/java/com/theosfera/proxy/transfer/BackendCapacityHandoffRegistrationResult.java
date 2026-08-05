f194b6ed0e4351899298d88fbd6e9eca1e52c567
HarriOcho <76268969+HarriOcho@users.noreply.github.com>
2026-08-04 21:21:32 -0500

    feat: define capacity handoff registration outcomes

---
+++ src/main/java/com/theosfera/proxy/transfer/BackendCapacityHandoffRegistrationResult.java
@@ -0,0 +1,8 @@
+package com.theosfera.proxy.transfer;
+
+public enum BackendCapacityHandoffRegistrationResult {
+    REGISTERED,
+    ALREADY_REGISTERED,
+    PLAYER_BUSY,
+    REQUEST_ID_CONFLICT
+}
